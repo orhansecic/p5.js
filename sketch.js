d3288ab344c45e191ca1a2070f3f803516ecb2e0
orhansecic <orhan.secic@stu.ssst.edu.ba>
2019-10-28 14:05:04 +0100

    First Sketch in p5.js - Circle + Color

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(400, 300);
+}
+
+function draw() {
+  background(255,204,100);
+  //rectMode(CENTER);
+  //rect(200, 150, 100, 100);
+  
+  noFill();
+  strokeWeight(5);
+  stroke(0, 0, 0)
+  circle(200, 150, 150);
+  stroke(255, 0, 0);
+  circle(200, 150, 120);
+  stroke(0, 0, 255);
+  circle(200, 150, 90);
+  stroke(255, 0, 0);
+  circle(200, 150, 60);
+  stroke(0, 0, 255);
+  circle(200, 150, 30);
+  stroke(0, 0, 0);
+  circle(200, 150, 0);
+  
+  
+      
+  
+}
